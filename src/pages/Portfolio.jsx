1061f965a6675a879fc22f05e63ca2e121c4eea5
Ace Perez <acenathen.perez@gmail.com>
2026-03-20 18:16:37 +0000

    File changes

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import Sidebar from '../components/portfolio/Sidebar';
 import MobileSidebar from '../components/portfolio/MobileSidebar';
 import HeroSection from '../components/portfolio/HeroSection';
